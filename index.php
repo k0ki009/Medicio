ffe6eddbcccb3793468e4e5d98b8dba1d91c109a
VerlyXd <janericaxskyzi@gmail.com>
2024-02-16 12:19:16 +0800

    GICALE WATAPAMPAM

--- index.php
+++ index.php
@@ -928,7 +928,7 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by GICALE WATAPAMPAM <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
   </footer><!-- End Footer -->
