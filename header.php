281210625f3cb662f5de0f9a92ef4833a3a6083e
Esabel <zndrsmbng@gmail.com>
2024-02-22 10:02:17 +0800

    ds

--- header.php
+++ header.php
@@ -58,7 +58,7 @@
                     <li><a class="nav-link scrollto" href="index.php">Home</a></li>
                     <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
                         <ul class="scrollable-menu">
-                            <li><a href="#">Director</a></li>
+                            <li><a href="leadership.php">Leadership</a></li>
                             <li><a href="#">Vision and Mission</a></li>
                         </ul>
                     </li>
